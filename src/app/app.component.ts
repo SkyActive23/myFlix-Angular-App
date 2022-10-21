5701307f12c18637f5778116d00b653a7180f22b
Sky <skyrj50031@maricopa.edu>
2022-10-20 18:56:22 -0700

    6.4 Commented out old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MatDialog } from '@angular/material/dialog';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+// import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
